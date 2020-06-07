e51f29fb3bc8ab77101f78cd81a06abe8bc6183d
MccDev260 <jjmcc2660@gmail.com>
2020-06-07 19:54:07 +0100

    Changed name notation to make script more concise

--- script.py
+++ script.py
@@ -1,21 +1,21 @@
 import os.path
 import shutil
 
-folder_path = "" # Invert slashes in folder path
+folder_path = " " # Invert slashes in folder path
 
-images = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
-for image in images:
-    folder_name = image.split(' ')[0] # Use this to choose how to sort files
+for file in files:
+    folder_name = file.split(' ')[0] # Use this to choose how to sort files
 
     new_path = os.path.join(folder_path, folder_name)
 
-    old_image_path = os.path.join(folder_path, image)
+    old_file_path = os.path.join(folder_path, file)
 
     if not os.path.exists(new_path):
         os.makedirs(new_path)
-        new_image_path = os.path.join(new_path, image)
-        shutil.move(old_image_path, new_image_path)
+        new_file_path = os.path.join(new_path, file)
+        shutil.move(old_file_path, new_file_path)
     
     else:
-        shutil.move(old_image_path, new_path)
+        shutil.move(old_file_path, new_path)
